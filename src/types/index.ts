1b884a00772ac73f319bf2b964da687a01225552
SanketDMane <ironmanmarks456@gmail.com>
2025-04-15 14:42:15 +0530

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,26 @@
-export interface Product {
+// export interface Product {
+//   id: string;
+//   name: string;
+//   description: string;
+//   price: number;
+//   imageUrl: string;
+//   category: string;
+// }
+
+// export interface Inquiry {
+//   id: string;
+//   productId: string;
+//   name: string;
+//   email: string;
+//   message: string;
+//   createdAt: Date;
+// }
+
+export type Product = {
   id: string;
   name: string;
   description: string;
   price: number;
   imageUrl: string;
   category: string;
-}
-
-export interface Inquiry {
-  id: string;
-  productId: string;
-  name: string;
-  email: string;
-  message: string;
-  createdAt: Date;
-}
+};
