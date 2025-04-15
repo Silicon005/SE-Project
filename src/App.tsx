e9dae03181dc86dd293637e181fccd56c54359ff
SanketDMane <ironmanmarks456@gmail.com>
2025-04-15 21:04:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Toaster } from 'react-hot-toast';
-// import { Navbar } from './components/Navbar';
-// import { Home } from './pages/Home';
-// import { Products } from './pages/Products';
-// import { ProductDetails } from './pages/ProductDetails';
-// import { AdminLogin } from './pages/AdminLogin';
-// import { AdminDashboard } from './pages/AdminDashboard';
-// import { AdminProducts } from './pages/AdminProducts';
-// import { AdminInquiries } from './pages/AdminInquiries';
-// import { AdminContacts } from './pages/AdminContacts';
-// import { AboutUs } from './pages/AboutUs'; 
-// import { Contact } from './pages/Contact';
-// import { Gallery } from './pages/Gallery';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Products } from './pages/Products';
+import { ProductDetails } from './pages/ProductDetails';
+import { AdminLogin } from './pages/AdminLogin';
+import { AdminDashboard } from './pages/AdminDashboard';
+import { AdminProducts } from './pages/AdminProducts';
+import { AdminInquiries } from './pages/AdminInquiries';
+import { AdminContacts } from './pages/AdminContacts';
+import { AboutUs } from './pages/AboutUs'; 
+import { Contact } from './pages/Contact';
+import { Gallery } from './pages/Gallery';
 
-// function App() {
-//   return (
-//     <Router>
-//       <div className="min-h-screen bg-gray-50">
-//         <Navbar />
-//         <Routes>
-//           {/* Existing routes */}
-//           <Route path="/" element={<Home />} />
-//           <Route path="/products" element={<Products />} />
-//           <Route path="/products/:id" element={<ProductDetails />} />
-//           <Route path="/admin" element={<AdminLogin />} />
-//           <Route path="/admin/dashboard" element={<AdminDashboard />} />
-//           <Route path="/admin/products" element={<AdminProducts />} />
-//           <Route path="/admin/inquiries" element={<AdminInquiries />} />
-//           <Route path="/admin/contacts" element={<AdminContacts />} />
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          {/* Existing routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/products" element={<AdminProducts />} />
+          <Route path="/admin/inquiries" element={<AdminInquiries />} />
+          <Route path="/admin/contacts" element={<AdminContacts />} />
 
           
-//           {/* New routes */}
-//           <Route path="/about" element={<AboutUs />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="/gallery" element={<Gallery />} />
-//         </Routes>
-//         <Toaster position="bottom-right" />
-//       </div>
-//     </Router>
-//   );
-// }
+          {/* New routes */}
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+        <Toaster position="bottom-right" />
+      </div>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
